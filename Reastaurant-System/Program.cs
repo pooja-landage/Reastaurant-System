b30d0ab67b0c3426c66f053bd6213dce48cc6b15
pooja-landage <poojalandage22@gmail.com>
2021-09-21 08:37:35 +0530

    Add project files.

---
+++ Reastaurant-System/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using RestaurantLogic;
+
+namespace Reastaurant_System
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Enter User Name");
+            string username = Console.ReadLine();
+            Console.WriteLine("Enter Password:");
+            string pass = Console.ReadLine();
+            Restaurant.Validation(username, pass);
+          
+        }
+    }
+}
